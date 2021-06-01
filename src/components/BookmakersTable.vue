507929ac290a9ed407ee09f962519efe551c8a93
Basskey <basskey@bk.ru>
2021-06-01 17:21:33 +0300

    feat: completed details section

--- src/components/BookmakersTable.vue
+++ src/components/BookmakersTable.vue
@@ -45,29 +45,52 @@
            v-bind:class="{ active: bookmaker.isActive }">
         <div class="mobile-selector">
           <span>На мобильных</span>
-          <div class="comparison-container">
-            <ul class="comparison-list">
-              <h3 class="comparison-list__title comparison-list__title--plus">Плюсы <span>5</span></h3>
-              <li>Приветственный бонус и высокие коэффициенты</li>
-              <li>Приветственный бонус</li>
-              <li>Приветственный бонус</li>
-              <li>Приветственный бонус</li>
-            </ul>
-            <ul class="comparison-list">
-              <h3 class="comparison-list__title comparison-list__title--minus">Минусы <span>5</span></h3>
-              <li>Приветственный бонус</li>
-              <li>Приветственный бонус</li>
-              <li>Приветственный бонус</li>
-            </ul>
-            <ul class="comparison-list">
-              <h3 class="comparison-list__title comparison-list__title--rating">Место в рейтингах</h3>
-              <li>Народный</li>
-              <li>Надёжность</li>
-              <li>Коэффициенты</li>
-              <li>Выбор ставок</li>
-              <li>Сервис LIVE</li>
-            </ul>
+        </div>
+        <div class="comparison-container">
+          <ul class="comparison-list">
+            <h3 class="comparison-list__title comparison-list__title--plus">Плюсы <span>5</span></h3>
+            <li>Приветственный бонус и высокие коэффициенты</li>
+            <li>Приветственный бонус</li>
+            <li>Приветственный бонус</li>
+            <li>Приветственный бонус</li>
+          </ul>
+          <ul class="comparison-list">
+            <h3 class="comparison-list__title comparison-list__title--minus">Минусы <span>5</span></h3>
+            <li>Приветственный бонус</li>
+            <li>Приветственный бонус</li>
+            <li>Приветственный бонус</li>
+          </ul>
+          <ul class="comparison-list">
+            <h3 class="comparison-list__title comparison-list__title--rating">Место в рейтингах</h3>
+            <li>Народный</li>
+            <li>Надёжность</li>
+            <li>Коэффициенты</li>
+            <li>Выбор ставок</li>
+            <li>Сервис LIVE</li>
+          </ul>
+        </div>
+        <div class="bonuses">
+          <div class="bonus">
+            <span>Страховка до 10 000 рублей</span>
+            <button class="bonus-cta">Получить бонус</button>
           </div>
+          <div class="bonus">
+            <span>Кешбэк до 15 000 рублей</span>
+            <button class="bonus-cta">Получить бонус</button>
+          </div>
+          <div class="bonus">
+            <span>Эксклюзивный бонус за депозит до 8 000 рублей</span>
+            <button class="bonus-cta">Получить бонус</button>
+          </div>
+        </div>
+        <div class="note">
+          <span>Данный букмекер сотрудничает с Fanler в решении спорных ситуаций</span>
+          <a class="note-link" href="">Добавить жалобу</a>
+        </div>
+        <div class="cta">
+          <button class="cta-btn">Начать играть</button>
+          <button class="read-btn">Читать обзор</button>
+          <a class="info" href="#">Как зарегистрироваться у букмекера BetCity</a>
         </div>
       </div>
     </div>
@@ -129,10 +152,6 @@
         ]
       }
     },
-
-    mounted() {
-      console.log(this.mockData)
-    }
   }
 </script>
 
@@ -274,11 +293,15 @@
 
       &--details {
         grid-area: details;
-        height: 0;
-        overflow: hidden;
+        flex-wrap: wrap;
+        display: none;
 
         &.active {
-          height: auto;
+          display: block;
+        }
+
+        .mobile-selector {
+          width: 100%;
         }
 
         .comparison-container {
@@ -344,6 +367,141 @@
               &:not(last-child) {
                 margin: 0 0 20px 0;
               }
+            }
+          }
+        }
+
+        .bonuses {
+
+          .bonus {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            height: 56px;
+            background:  #FFF5CF url("/img/gift.svg") no-repeat;
+            background-position: 16px;
+            border-radius: 8px;
+            padding: 4px 4px 4px 56px;
+            margin: 0 0 8px 0;
+            &:last-of-type {
+              margin: 0 0 16px 0;
+            }
+
+            .bonus-cta {
+              width: 174px;
+              height: 48px;
+              background: #1C1D1F;
+              border: none;
+              border-radius: 8px;
+              display: flex;
+              align-items: center;
+              justify-content: center;
+              font-family: 'Raleway', sans-serif;
+              font-style: normal;
+              font-weight: bold;
+              font-size: 16px;
+              line-height: 1.5em;
+              letter-spacing: 0.6px;
+              font-feature-settings: 'pnum' on, 'lnum' on, 'liga' off;
+              color: #FFFFFF;
+              text-decoration: none;
+            }
+          }
+        }
+
+        .note {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          height: 72px;
+          border: 1px solid #E2E4E8;
+          border-radius: 8px;
+          padding: 16px 16px 16px 56px;
+          background: url("/img/icon-note.svg") no-repeat 16px center;
+          margin: 0 0 32px 0;
+
+          &-link {
+            display: block;
+            flex-shrink: 0;
+            font-family: 'Raleway', sans-serif;
+            font-style: normal;
+            font-weight: normal;
+            font-size: 16px;
+            line-height: 2em;
+            text-decoration-line: underline;
+            font-feature-settings: 'pnum' on, 'lnum' on;
+            color: #1C1D1F;
+            &:hover,
+            &:active {
+              color: #1C1D1F;
+            }
+          }
+        }
+
+        .cta {
+          display: flex;
+
+          &-btn {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            width: 159px;
+            height: 48px;
+            background: #E95836;
+            border-radius: 8px;
+            border: none;
+            color: #FFFFFF;
+            font-family: 'Raleway', sans-serif;
+            font-style: normal;
+            font-weight: bold;
+            font-size: 16px;
+            line-height: 1.5em;
+            letter-spacing: 0.6px;
+            font-feature-settings: 'pnum' on, 'lnum' on, 'liga' off;
+          }
+
+          .read-btn {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            width: 159px;
+            height: 48px;
+            background: #F3F6F9;
+            border-radius: 8px;
+            border: none;
+            color: #1C1D1F;
+            font-family: 'Raleway', sans-serif;
+            font-style: normal;
+            font-weight: bold;
+            font-size: 16px;
+            line-height: 1.5em;
+            letter-spacing: 0.6px;
+            font-feature-settings: 'pnum' on, 'lnum' on, 'liga' off;
+            margin: 0 auto 0 8px;
+          }
+
+          .info {
+            font-family: 'Raleway', sans-serif;
+            font-style: normal;
+            font-weight: normal;
+            font-size: 14px;
+            line-height: 1.7em;
+            font-feature-settings: 'pnum' on, 'lnum' on;
+            color: #1C1D1F;
+            display: flex;
+            align-items: center;
+            &:hover,
+            &:active {
+              color: #1C1D1F;
+            }
+            &::before {
+              content: '';
+              background: url("/img/icon-info.svg") no-repeat center;
+              background-size: contain;
+              width: 14px;
+              height: 15px;
+              margin: 0 10px 0 0;
+              display: block;
             }
           }
         }
